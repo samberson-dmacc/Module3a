44bee455231c6534d0b088e17bc96ed060eca8a5
Sarah <samberson@dmacc.edu>
2019-09-10 17:10:09 -0500

    Test1

--- test/test.py
+++ test/test.py
@@ -11,6 +11,7 @@ if __name__ == '__main__':
     unittest.main()
 
 
+
 def test_average(self):
     with mock.patch('builtins.input', side_effect=[85, 90, 95]):
         assert topic2.average() == 90
